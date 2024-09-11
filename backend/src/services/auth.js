60f1d483a3d85b91087d7888721d50cf6688c980
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-09-11 19:40:39 -0300

    feat: add função checkMembroExistsByEmail  ao arquivo de services

--- backend/src/services/auth.js
+++ backend/src/services/auth.js
@@ -1,19 +1,32 @@
 const { Membro } = require("../models");
+const ConflictError = require("../errors/conflictError");
 
 const createMembro = async (data) => {
-  console.log(Membro);
-  console.log(data);
-  const membro = await Membro.create(
-    {
-      nome: data.nome,
-      email: data.email,
-      senha: data.senha,
-    },
-  );
+  const membro = await Membro.create({
+    nome: data.nome,
+    email: data.email,
+    senha: data.senha,
+  });
 
-  return membro
+  return membro;
+};
+
+const checkMembroExistsByEmail = async (email) => {
+  const membro = await Membro.findOne({
+    where: {
+      email: email,
+    },
+    attributes: ["id", "nome", "email"],
+  });
+
+  if (membro) {
+    throw new ConflictError("Membro já cadastrado!");
+  }
+
+  return membro;
 };
 
 module.exports = {
   createMembro,
+  checkMembroExistsByEmail,
 };
