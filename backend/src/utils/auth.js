4b0970a4798a8037568f84b2fe701bc57aaa95a2
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-09-11 20:38:08 -0300

    feat: add funções verifyPassword e generateJWTToken ao arquivo de utils/auth.js

--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -1,24 +1,59 @@
-const ValidationError = require("../errors/validationError")
+const ValidationError = require("../errors/validationError");
+const AuthenticationError = require("../errors/authenticationError");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const validateDataRequest = (req) => {
+const validateDataRequest = (req, method) => {
   const membroData = {};
   membroData.nome = req.body.nome;
   membroData.email = req.body.email;
   membroData.senha = req.body.senha;
 
-  if (!membroData.nome || !membroData.email || !membroData.senha) {
-    throw new ValidationError("Campos inválidos e/ou não preenchidos");
+  if (method === "register") {
+    if (!membroData.nome || !membroData.email || !membroData.senha) {
+      throw new ValidationError("Campos inválidos e/ou não preenchidos");
+    }
+    return membroData;
+  } else if (method === "login") {
+    if (!membroData.email || !membroData.senha) {
+      throw new ValidationError("Campos inválidos e/ou não preenchidos");
+    }
+    return membroData;
   }
-  return membroData;
 };
 
 const generateHashedPassword = async (senha) => {
   hashedPassword = await bcrypt.hash(senha, 10);
   return hashedPassword;
-}
+};
+
+const verifyPassword = async (inputPassword, rightPassword) => {
+  const passwordIsValid = await bcrypt.compare(inputPassword, rightPassword);
+  if (!passwordIsValid) {
+    throw new AuthenticationError("Senha inválida!");
+  }
+  return passwordIsValid;
+};
+
+const generateJWTToken = async (membro) => {
+  const token = jwt.sign(
+    {
+      id: membro.id,
+      nome: membro.nome,
+      email: membro.email,
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: process.env.JWT_REFRESH_EXPIRATION,
+    }
+  );
+  return token;
+};
 
 module.exports = {
   validateDataRequest,
-  generateHashedPassword
-}
+  generateHashedPassword,
+  verifyPassword,
+  generateJWTToken,
+};
