ed46a86e3ed0dae33361bfeafaf98d4810b0a45e
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-09-11 20:39:08 -0300

    feat: add função membroAuthenticate para autenticação do membro ao arquivo de services/auth.js

--- backend/src/services/auth.js
+++ backend/src/services/auth.js
@@ -1,6 +1,7 @@
 const { Membro } = require("../models");
 const ConflictError = require("../errors/conflictError");
-const { generateHashedPassword } = require("../utils/auth");
+const NotFoundError = require("../errors/notFoundError");
+const { generateHashedPassword, verifyPassword } = require("../utils/auth");
 
 const createMembro = async (data) => {
   const hashedPassword = await generateHashedPassword(data.senha);
@@ -13,22 +14,35 @@ const createMembro = async (data) => {
   return membro;
 };
 
-const checkMembroExistsByEmail = async (email) => {
+const findMembroByEmail = async (email) => {
   const membro = await Membro.findOne({
     where: {
       email: email,
-    },
-    attributes: ["id", "nome", "email"],
+    }
   });
+  return membro;
+};
+
+const checkMembroExistsByEmail = async (email) => {
+  const membro = await findMembroByEmail(email);
 
   if (membro) {
     throw new ConflictError("Membro já cadastrado!");
   }
+  return membro;
+};
 
+const membroAuthenticate = async (data) => {
+  const membro = await findMembroByEmail(data.email);
+  if (!membro) {
+    throw new NotFoundError("Usúario não é registrado");
+  }
+  await verifyPassword(data.senha, membro.senha);
   return membro;
 };
 
 module.exports = {
   createMembro,
   checkMembroExistsByEmail,
+  membroAuthenticate,
 };
