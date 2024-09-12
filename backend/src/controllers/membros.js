56e66c73a0c284fb3f1f0d26b16ad2f040b3e4f5
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-09-12 12:04:13 -0300

    feat: add controller updateMembro

--- backend/src/controllers/membros.js
+++ backend/src/controllers/membros.js
@@ -1,5 +1,10 @@
 const { sendSuccessResponse, sendErrorResponse } = require("../utils/utils");
-const { getAllMembros, deleteMembroById } = require("../services/membros");
+const { validateDataRequest, updateTokenSign } = require("../utils/auth");
+const {
+  getAllMembros,
+  deleteMembroById,
+  updateDataMembro,
+} = require("../services/membros");
 
 const getAll = async (req, res) => {
   try {
@@ -11,12 +16,29 @@ const getAll = async (req, res) => {
 };
 
 const deleteMembro = async (req, res) => {
-  const membroId = req.membro.id;
-  const membro = await deleteMembroById(membroId);
-  sendSuccessResponse(res, "Membro deletado", membro);
+  try {
+    const membroId = req.membro.id;
+    await deleteMembroById(membroId);
+    sendSuccessResponse(res, "Membro deletado", membroId);
+  } catch (error) {
+    sendErrorResponse(res, error);
+  }
+};
+
+const updateMembro = async (req, res) => {
+  try {
+    const membro = validateDataRequest(req, "register");
+    membro.id = req.membro.id;
+
+    await updateDataMembro(membro);
+    sendSuccessResponse(res, "Membro atualizado", membro.email);
+  } catch (error) {
+    sendErrorResponse(res, error);
+  }
 };
 
 module.exports = {
   getAll,
-  deleteMembro
+  deleteMembro,
+  updateMembro,
 };
