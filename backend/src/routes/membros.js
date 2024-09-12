85976cea7784ac124173cf82c1b681f4ea1d7eab
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-09-12 10:57:01 -0300

    feat: add rota delete para controller deleteMembro

--- backend/src/routes/membros.js
+++ backend/src/routes/membros.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { authenticateToken } = require("../middlewares/authMiddleware");
 const membros = require("../controllers/membros");
 
-router.get("/", authenticateToken, membros.getMembros);
+router.get("/", authenticateToken, membros.getAll);
+router.delete("/", authenticateToken, membros.deleteMembro);
 
 module.exports = router;
