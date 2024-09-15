3472f0f8220c89263e2838abb2f63a655c46291c
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-09-15 01:00:32 -0300

    feat: add lógica para mostrar edições ao membro dono da tarefa

--- frontend/src/pages/createTarefa/createTarefa.js
+++ frontend/src/pages/createTarefa/createTarefa.js
@@ -47,7 +47,6 @@ const CreateTarefa = () => {
             className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500"
             value={descricao}
             onChange={(e) => setDescricao(e.target.value)}
-            required
           />
         </div>
 
